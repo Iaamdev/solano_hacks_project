f8a332860ac28279643f20f47355391f7fc956a5
SeriousCoffeeCup <seriouscoffeecup@gmail.com>
2024-11-09 14:06:38 -0800

    Graph prerequisites as tree using NetworkX

--- Coffee_Grounds/0_RAG/openai_prompts.py
+++ Coffee_Grounds/0_RAG/openai_prompts.py
@@ -4,8 +4,9 @@ import csv
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-sysrole = "Engineer"
-prompt = "Tell me what to do to learn git"
+sysrole = "You're a pro chef teaching beginners"
+prompt = "I want to cook steak"
+suffix = "  For the instructions you output, output a csv format where each step of the recipe is a row with Recipe Name, Step Name, Step Description, a prerequisite step ( if any ), a subsequent step ( if any ), and Duration ( lower bound in minutes ) and Duration ( upper bound in minutes ) such that 5-10 minutes would be 5 under Duration ( lower bound in minutes ) and 10 under Duration ( upper bound in minutes )"
 
 
 response = openai.chat.completions.create(
